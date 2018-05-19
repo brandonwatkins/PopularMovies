10976a77bc50f00ee91d5b19a0c4f5cadeef60b6
Brandon Watkins <brandonwatkins1@mhu.edu>
2018-05-19 10:16:06 -0400

    Stubbed out onclickmethods and created a picasso request

--- app/src/main/java/com/example/android/popularmovies/MainActivity.java
+++ app/src/main/java/com/example/android/popularmovies/MainActivity.java
@@ -2,12 +2,33 @@ package com.example.android.popularmovies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.GridView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private GridView mGridView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mGridView = (GridView)findViewById(R.id.gridView);
+        final movieUtils movieUtils = new movieUtils(this, movies);
+        mGridView.setAdapter(movieUtils);
+
+        mGridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, android.view.View view, int position, long id) {
+                Movie movie = movies[position];
+                //movie.toggleFavorite();
+
+                // This tells the GridView to redraw itself
+                // in turn calling your MovieAdapter's getView method again for each cell
+                //movieUtils.notifyDataSetChanged();
+            }
+        });
     }
 }
--- app/src/main/java/com/example/android/popularmovies/movieUtils.java
+++ app/src/main/java/com/example/android/popularmovies/movieUtils.java
@@ -1,11 +1,59 @@
 package com.example.android.popularmovies;
 
 import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.squareup.picasso.Picasso;
 
 public class movieUtils {
 
     private Context mContext;
     private Movie[] mMovies;
 
+    public movieUtils (Context context, Movie[] movies) {
+        this.mContext = context;
+        this.mMovies = movies;
+    }
 
+    @Override
+    public int getCount() {
+        return mMovies.length;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        final Movie movie = mMovies[position];
+        if (convertView == null) {
+            final LayoutInflater layoutInflater = LayoutInflater.from(mContext);
+            convertView = layoutInflater.inflate(R.layout.movie_poster, null);
+        }
+        final ImageView imageView = (ImageView)convertView.findViewById(R.id.poster);
+        //final TextView nameTextView = (TextView)convertView.findViewById(R.id.textview_movie_name);
+        //final ImageView imageViewFavorite = (ImageView)convertView.findViewById(R.id.imageview_favorite);
+
+        Picasso.with(mContext)
+                .load(movie.getUrl()) //Load the image
+                //.placeholder(R.drawable.ic_placeholder) //Image resource that act as placeholder
+                //.error(R.drawable.ic_error) //Image resource for error
+                .resize(300, 500)  // Post processing - Resizing the image
+                .into(imageView); // View where image is loaded.
+
+        //nameTextView.setText(movie.getName());
+
+        return convertView;
+    }
 }
